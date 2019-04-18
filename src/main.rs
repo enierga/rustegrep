667ba0a95dfc5c9e56a242b23757336db713c88c
Vincent Enierga <venierga@email.unc.edu>
2019-04-18 14:22:26 +0000

    kale salad

--- src/main.rs
+++ src/main.rs
@@ -62,7 +62,8 @@ fn eval(input: &str, options: &Options) {
         eval_tokens(input);
     }
 
-    let mut mod_input = String::new();
+    // modifying input to allow pattern matching within strings
+    let mut mod_input = String::from("(.*)");
     mod_input.push_str(input);
     mod_input.push_str("(.*)");
 
